4ed2cce3f6c3ee0392131f7fefa8d8f0c1bda206
Anselm Munango <amunango2004@gmail.com>
2026-07-08 15:16:41 +0200

    Refactor Program and Resume classes to improve structure and organization

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -1,3 +1,10 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
 Job job1 = new Job();
 job1._jobTitle = "Software Engineer";
 job1._company = "Microsoft";
@@ -16,3 +23,6 @@ myResume._jobs.Add(job1);
 myResume._jobs.Add(job2);
 
 myResume.Display();
+
+    }
+}
